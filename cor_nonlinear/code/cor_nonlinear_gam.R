086a40b24acd02a7b9f74d6cc61cf5653b0e1e08
Youyi Fong <youyifong@gmail.com>
2021-10-21 18:46:09 -0700

    update llods to lloxs; update cor_nonlinear

--- cor_nonlinear/code/cor_nonlinear_gam.R
+++ cor_nonlinear/code/cor_nonlinear_gam.R
@@ -131,7 +131,7 @@ for (w.wo.plac in 1:2) { # 1 with placebo lines, 2 without placebo lines. Implem
         plot(prob~marker, risks, xlab=labels.assays.short[a]%.%ifelse(ii==1," (=s)"," (>=s)"), xlim=xlim, 
             ylab=paste0("Probability* of ",config.cor$txt.endpoint," by Day ", tfinal.tpeak), lwd=lwd, ylim=ylim, type="n", main=paste0(labels.assays.long["Day"%.%tpeak,a]), xaxt="n")
     
-        draw.x.axis.cor(xlim, llods[a])
+        draw.x.axis.cor(xlim, lloxs[a])
     
         # prevelance lines
         abline(h=prev.plac, col="gray", lty=c(1,3,3), lwd=lwd)
