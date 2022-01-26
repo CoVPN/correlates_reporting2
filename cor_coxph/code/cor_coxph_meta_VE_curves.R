66138e2f971ced1294ac92e8c2b00c9bcd89ba7a
Youyi Fong <youyifong@gmail.com>
2022-01-25 19:06:11 -0800

    updates

--- cor_coxph/code/cor_coxph_meta_VE_curves.R
+++ cor_coxph/code/cor_coxph_meta_VE_curves.R
@@ -24,14 +24,14 @@ if (!dir.exists(save.results.to))  dir.create(save.results.to)
 
 # TRIALS is a subset of all.trials
 # a is an assay
-draw.ve.curves=function(a, TRIALS, file.name, include.az=FALSE) {
-#a="bindSpike"; TRIALS=c("moderna_real", "janssen_pooled_real"); file.name=1
+draw.ve.curves=function(a, TRIALS, file.name, include.az=FALSE, log="") {
+#a="pseudoneutid50"; TRIALS=c("moderna_real", "janssen_pooled_real"); file.name=1; include.az=T; log=""
     myprint(a)
-    ylim=c(0, 1)    
+    if(log=="") ylim=c(0, 1) else ylim=c(0.1, 1)
     hist.shrink=1/c(ADCP=2,pseudoneutid50=1.2,bindSpike=1.3,bindRBD=1.3)
     
     all.trials=c("moderna_real", "janssen_pooled_real", "janssen_na_real", "janssen_la_real", "janssen_sa_real", "AZ-COV002")
-    studies=c("COVE","ENSEMBLE","ENSEMBLE NA","ENSEMBLE LA","ENSEMBLE SA","AZ-COV002"); names(studies)=all.trials
+    studies=c("COVE","ENSEMBLE","ENSEMBLE US","ENSEMBLE LA","ENSEMBLE SA","AZ-COV002"); names(studies)=all.trials
     cols=  c("blue","green","green","olivedrab3","darkseagreen4","orange"); names(cols)=all.trials
     hist.col.ls=lapply(cols, function(col) {hist.col <- c(col2rgb(col)); rgb(hist.col[1], hist.col[2], hist.col[3], alpha=255*0.3, maxColorValue=255)})
     
@@ -45,7 +45,6 @@ draw.ve.curves=function(a, TRIALS, file.name, include.az=FALSE) {
     weight=list()
     for (x in TRIALS) {    
         TRIAL=get.trial(x, a)
-        myprint(TRIAL)
         Sys.setenv("TRIAL"=TRIAL)
         COR = ifelse (x=="moderna_real","D57","D29IncludeNotMolecConfirmedstart1")
         # key to have local = T
@@ -66,7 +65,7 @@ draw.ve.curves=function(a, TRIALS, file.name, include.az=FALSE) {
     xlim=c(min(sapply(xlim.ls, function(x) x[1])), max(sapply(xlim.ls, function(x) x[2])))
     myprint(xlim)
     
-    mypdf(file=paste0("output/meta/meta_controlled_ve_curves_",file.name,"_",a))
+    mypdf(file=paste0("output/meta/meta_controlled_ve_curves",ifelse(log=="","","log"),"_",file.name,"_",a), width=5.2, height=5.2)
         par(las=1, cex.axis=0.9, cex.lab=1)# axis label orientation
         
         # need several variables from sourcing _common.R: lloxs, labels.assays, draw.x.axis.cor
@@ -85,7 +84,7 @@ draw.ve.curves=function(a, TRIALS, file.name, include.az=FALSE) {
             shown=risks$marker>=ifelse(x=="moderna_real",log10(10),quantile(markers.x[[x]], 2.5/100, na.rm=T)) & risks$marker<=quantile(markers.x[[x]], 1-2.5/100, na.rm=T)
             mymatplot(risks$marker[shown], t(rbind(est, ci.band))[shown,], type="l", lty=c(1,3,3), lwd=2.5, make.legend=F, col=cols[x], ylab=paste0("Controlled VE"), xlab=labels.assays.short[a]%.%" (=s)", 
                 #main=paste0(labels.assays.long["Day"%.%tpeak,a]),
-                ylim=ylim, xlim=xlim, yaxt="n", xaxt="n", draw.x.axis=F, add=x!=TRIALS[1])
+                ylim=ylim, xlim=xlim, yaxt="n", xaxt="n", draw.x.axis=F, add=x!=TRIALS[1], log=log)
             draw.x.axis.cor(xlim, NA)
             yat=seq(-1,1,by=.1)
             axis(side=2,at=yat,labels=(yat*100)%.%"%")            
@@ -123,16 +122,31 @@ draw.ve.curves=function(a, TRIALS, file.name, include.az=FALSE) {
 # COVE + ENSEMBLE + AZ
 for (a in c("pseudoneutid50","bindSpike","bindRBD")) {
     draw.ve.curves(a, TRIALS=c("moderna_real", "janssen_pooled_real"), file.name="1", include.az=T)
+    draw.ve.curves(a, TRIALS=c("moderna_real", "janssen_pooled_real"), file.name="1", include.az=T, log="y")
 }
 
 
 # COVE + ENSEMBLE regions
-for (a in c("pseudoneutid50","bindSpike","bindRBD","ADCP")) {
-    draw.ve.curves(a, TRIALS=c(if(a!="ADCP") "moderna_real", "janssen_na_real", "janssen_la_real", "janssen_sa_real"), file.name="2", include.az=F)
+for (a in c("pseudoneutid50","bindSpike","bindRBD")) {
+    draw.ve.curves(a, TRIALS=c("moderna_real", "janssen_na_real", "janssen_la_real", "janssen_sa_real"), file.name="2", include.az=F)
+    draw.ve.curves(a, TRIALS=c("moderna_real", "janssen_na_real", "janssen_la_real", "janssen_sa_real"), file.name="2", include.az=F, log="y")
 }
 
 
 # ENSEMBLE regions
 for (a in c("pseudoneutid50","bindSpike","bindRBD","ADCP")) {
     draw.ve.curves(a, TRIALS=c("janssen_na_real", "janssen_la_real", "janssen_sa_real"), file.name="3", include.az=F)
+    draw.ve.curves(a, TRIALS=c("janssen_na_real", "janssen_la_real", "janssen_sa_real"), file.name="3", include.az=F, log="y")
+}
+
+# COVE + ENSEMBLE/US + AZ
+for (a in c("pseudoneutid50","bindSpike","bindRBD")) {
+    draw.ve.curves(a, TRIALS=c("moderna_real", "janssen_na_real"), file.name="4", include.az=T)
+    draw.ve.curves(a, TRIALS=c("moderna_real", "janssen_na_real"), file.name="4", include.az=T, log="y")
+}
+
+# COVE + ENSEMBLE/US
+for (a in c("pseudoneutid50","bindSpike","bindRBD")) {
+    draw.ve.curves(a, TRIALS=c("moderna_real", "janssen_na_real"), file.name="5", include.az=F)
+    draw.ve.curves(a, TRIALS=c("moderna_real", "janssen_na_real"), file.name="5", include.az=F, log="y")
 }
