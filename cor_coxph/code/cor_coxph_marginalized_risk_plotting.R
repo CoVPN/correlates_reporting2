dcd06c17bd0faa00f153a618241ec2e77f7c52de
Youyi Fong <youyifong@gmail.com>
2022-02-16 09:14:01 -0800

    updates

--- cor_coxph/code/cor_coxph_marginalized_risk_plotting.R
+++ cor_coxph/code/cor_coxph_marginalized_risk_plotting.R
@@ -128,7 +128,7 @@ for (a in assays) {
 # 3 same as 1 except that no sens curve is shown
 # 4 same as 3 except that y axis on -log(1-) scale
 for (eq.geq in 1:4) {  
-# eq.geq=2; a=assays[1]
+# eq.geq=3; a=assays[1]
 
     outs=lapply (assays, function(a) {        
         tmp=ifelse(eq.geq==1,"_eq",ifelse(eq.geq==2,"_geq","_eq_manus")); if(eq.geq==4) tmp=4
@@ -200,8 +200,8 @@ for (eq.geq in 1:4) {
         
                 
             # CVE
-            est = 1 - risks$prob/res.plac.cont["est"]
-            boot = 1 - t( t(risks$boot)/res.plac.cont[2:(1+ncol(risks$boot))] )                         
+            est = 1 - risks$prob/res.plac.cont["est"]; boot = 1 - t( t(risks$boot)/res.plac.cont[2:(1+ncol(risks$boot))] )
+            #est = 1 - (risks$prob+0.00227)/res.plac.cont["est"]; boot = 1 - t( t(risks$boot+0.00227)/res.plac.cont[2:(1+ncol(risks$boot))] )
             ci.band=apply(boot, 1, function (x) quantile(x, c(.025,.975)))  
             y= t(rbind(est, ci.band))[.subset,]
             if(eq.geq==4) y=-log(1-y)
