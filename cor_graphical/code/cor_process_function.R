2261d7aa82822baa2220d2fb0a367eb25b69ebc0
Yiwen Lu <kylielyw@gmail.com>
2022-03-16 16:26:17 -0700

    update rounding methods

--- cor_graphical/code/cor_process_function.R
+++ cor_graphical/code/cor_process_function.R
@@ -76,10 +76,10 @@ get_resp_by_group <- function(dat=dat, group=group){
     group_by_at(group) %>%
     mutate(counts = n(),
            counts_severe = sum(severe, na.rm=T),
-           num = round(sum(response * ifelse(!cohort_event %in% c("Post-Peak Cases", "Non-Cases"), 1, !!as.name(wt)), na.rm=T), 1), # for intercurrent cases, we don't need to adjust for the weight because all of them are from the same stratum
-           num_severe = round(sum(response * ifelse(!cohort_event %in% c("Post-Peak Cases", "Non-Cases"), 1, !!as.name(wt)) & severe==1, na.rm=T), 1),
-           denom = round(sum(ifelse(!cohort_event %in% c("Post-Peak Cases", "Non-Cases"), 1, !!as.name(wt)), na.rm=T), 1),
-           denom_severe = round(sum(ifelse(!cohort_event %in% c("Post-Peak Cases", "Non-Cases"), 1, !!as.name(wt)) & severe==1, na.rm=T), 1),
+           num = sum(response * ifelse(!cohort_event %in% c("Post-Peak Cases", "Non-Cases"), 1, !!as.name(wt)), na.rm=T), # for intercurrent cases, we don't need to adjust for the weight because all of them are from the same stratum
+           num_severe = sum(response * ifelse(!cohort_event %in% c("Post-Peak Cases", "Non-Cases"), 1, !!as.name(wt)) & severe==1, na.rm=T),
+           denom = sum(ifelse(!cohort_event %in% c("Post-Peak Cases", "Non-Cases"), 1, !!as.name(wt)), na.rm=T),
+           denom_severe = sum(ifelse(!cohort_event %in% c("Post-Peak Cases", "Non-Cases"), 1, !!as.name(wt)) & severe==1, na.rm=T),
            N_RespRate = paste0(counts, "\n",round(num/denom*100, 1),"%"),
            N_RespRate_severe = paste0(counts_severe, "\n",round(num_severe/denom_severe*100, 1),"%"),
            min = min(value),
