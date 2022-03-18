184360f9f460d988e01fae2a8e077462c2b69c74
Youyi Fong <youyifong@gmail.com>
2022-03-18 11:42:45 -0700

    updates

--- cor_coxph/code/cor_coxph_notrun.R
+++ cor_coxph/code/cor_coxph_notrun.R
@@ -153,6 +153,32 @@ tmp=subset(dat.vac.seroneg.id50, ph2==1, select=c(EventIndPrimary, EventTimePrim
 tail(tmp[order(tmp[,2]),], 50)
 
 
+tmp=subset(dat.vac.seroneg, ph2==1 & EventIndPrimary==1, select=c(EventIndPrimary, EventTimePrimary))
+tmp[order(tmp[,2]),] # last 48
+
+tmp=subset(dat.vac.seroneg, ph2==1 & EventIndPrimary==1 & Region==0, select=c(EventIndPrimary, EventTimePrimary))
+tmp[order(tmp[,2]),] # last 47
+
+tmp=subset(dat.vac.seroneg, ph2==1 & EventIndPrimary==1 & Region==1, select=c(EventIndPrimary, EventTimePrimary))
+tmp[order(tmp[,2]),] # last 48
+
+tmp=subset(dat.vac.seroneg, ph2==1 & EventIndPrimary==1 & Region==2, select=c(EventIndPrimary, EventTimePrimary))
+tmp[order(tmp[,2]),] # last 26
+
+
+tmp=subset(dat.vac.seroneg, ph2==1 & EventIndPrimary==0, select=c(EventIndPrimary, EventTimePrimary))
+tail(tmp[order(tmp[,2]),], 15) # last 54, first 54
+
+tmp=subset(dat.vac.seroneg, ph2==1 & EventIndPrimary==0 & Region==0, select=c(EventIndPrimary, EventTimePrimary))
+tail(tmp[order(tmp[,2]),], 15) # last 54, first 53
+
+tmp=subset(dat.vac.seroneg, ph2==1 & EventIndPrimary==0 & Region==1, select=c(EventIndPrimary, EventTimePrimary))
+tail(tmp[order(tmp[,2]),], 15) # last 54, first 49
+
+tmp=subset(dat.vac.seroneg, ph2==1 & EventIndPrimary==0 & Region==2, select=c(EventIndPrimary, EventTimePrimary))
+tail(tmp[order(tmp[,2]),], 15) # last 48, first 40
+
+
 tmp=subset(dat.vac.seroneg.id50.na, ph2==1, select=c(EventIndPrimary, EventTimePrimary))
 tmp[order(tmp[,2]),]
 # case 66 42
