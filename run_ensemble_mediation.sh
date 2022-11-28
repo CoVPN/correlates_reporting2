f3f18343c55baaee15aad62c356431e1a39bd0c9
dbenkes <dbenkes@bioc02wg0k1hx8f.wireless.emory.edu>
2022-11-28 14:16:02 -0500

    udpate

--- run_ensemble_mediation.sh
+++ run_ensemble_mediation.sh
@@ -1,13 +1,13 @@
 for TRIAL in janssen_pooled_partA  janssen_la_partA
 do
     sbatch --export=TRIAL=${TRIAL} \
-           --wrap="cd cop_mediation && Rscript code/cor_coxph.R D29IncludeNotMolecConfirmed"
+           --wrap="cd cop_mediation && Rscript code/cop_mediation.R D29IncludeNotMolecConfirmed"
     sbatch --export=TRIAL=${TRIAL} \
-           --wrap="cd cop_mediation && Rscript code/cor_coxph.R D29SevereIncludeNotMolecConfirmed"
+           --wrap="cd cop_mediation && Rscript code/cop_mediation.R D29SevereIncludeNotMolecConfirmed"
 done
 
 for TRIAL in janssen_pooled_partAsenior janssen_pooled_partAnonsenior janssen_na_partA janssen_na_partAsenior janssen_na_partAnonsenior janssen_la_partAsenior janssen_la_partAnonsenior janssen_sa_partA janssen_sa_partAnonsenior
 do 
     sbatch --export=TRIAL=${TRIAL} \
-           --wrap="cd cop_mediation && Rscript code/cor_coxph.R D29IncludeNotMolecConfirmed"
+           --wrap="cd cop_mediation && Rscript code/cop_mediation.R D29IncludeNotMolecConfirmed"
 done
