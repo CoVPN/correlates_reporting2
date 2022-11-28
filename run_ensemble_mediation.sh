b476d70d17d5c4d4d3e679a54bc871bc946ac8fc
dbenkes <dbenkes@bioc02wg0k1hx8f.wireless.emory.edu>
2022-11-28 14:13:43 -0500

    update shell script

--- run_ensemble_mediation.sh
+++ run_ensemble_mediation.sh
@@ -1,4 +1,13 @@
-for TRIAL in janssen_pooled_partA janssen_na_partA janssen_la_partA janssen_sa_partA
+for TRIAL in janssen_pooled_partA  janssen_la_partA
 do
-    sbatch --export=TRIAL=${TRIAL} --wrap "make -k -C cop_mediation all"
+    sbatch --export=TRIAL=${TRIAL} \
+           --wrap="cd cop_mediation && Rscript code/cor_coxph.R D29IncludeNotMolecConfirmed"
+    sbatch --export=TRIAL=${TRIAL} \
+           --wrap="cd cop_mediation && Rscript code/cor_coxph.R D29SevereIncludeNotMolecConfirmed"
+done
+
+for TRIAL in janssen_pooled_partAsenior janssen_pooled_partAnonsenior janssen_na_partA janssen_na_partAsenior janssen_na_partAnonsenior janssen_la_partAsenior janssen_la_partAnonsenior janssen_sa_partA janssen_sa_partAnonsenior
+do 
+    sbatch --export=TRIAL=${TRIAL} \
+           --wrap="cd cop_mediation && Rscript code/cor_coxph.R D29IncludeNotMolecConfirmed"
 done
