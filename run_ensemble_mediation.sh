e4a3a00deeb049a973823f1bd7ae93fff75edd0b
David Benkeser <dbenkeser@gmail.com>
2023-08-10 12:00:45 -0400

    update ensemble severe disease run

--- run_ensemble_mediation.sh
+++ run_ensemble_mediation.sh
@@ -1,85 +1,51 @@
+    Rscript code/cor_coxph.R D29IncludeNotMolecConfirmed
+    Rscript code/cor_coxph.R D29SevereIncludeNotMolecConfirmed
+    Rscript code/cor_coxph.R D29ModerateIncludeNotMolecConfirmed
+
+    janssen_pooled_partA  janssen_na_partA janssen_la_partA janssen_sa_partA 
+
 for run_survtmle in TRUE FALSE
-do
-for impute_placebo in TRUE FALSE
-do
-# Geog regions pooled COVID-19 endpoint: 4 markers, for Overall, Senior, and non-Senior
-for TRIAL in janssen_pooled_partA janssen_pooled_partAsenior janssen_pooled_partAnonsenior
-do
-    for MARKER in Day29bindSpike Day29bindRBD Day29pseudoneutid50 Day29ADCP
+  do
+  for impute_placebo in TRUE FALSE
     do
-    sbatch --export=TRIAL=${TRIAL} \
+    for outcome in D29IncludeNotMolecConfirmed D29SevereIncludeNotMolecConfirmed D29ModerateIncludeNotMolecConfirmed
+      do
+      # POOLED
+      for TRIAL in janssen_pooled_partA
+        do
+        for MARKER in Day29bindSpike Day29bindRBD Day29pseudoneutid50
+          do
+          sbatch --export=TRIAL=${TRIAL} \
+          --wrap="cd ~/correlates_reporting2/cop_mediation && /app/software/R/4.0.4-foss-2020b/bin/Rscript code/mediation.R ${outcome} ${run_survtmle} ${impute_placebo} ${MARKER}"
+        done
+      done
+      # LATIN AMERICA
+      for TRIAL in janssen_la_partA
+        do
+        for MARKER in Day29bindSpike Day29bindRBD Day29pseudoneutid50 Day29pseudoneutid50la
+          do
+          sbatch --export=TRIAL=${TRIAL} \
            --wrap="cd ~/correlates_reporting2/cop_mediation && /app/software/R/4.0.4-foss-2020b/bin/Rscript code/mediation.R D29IncludeNotMolecConfirmed ${run_survtmle} ${impute_placebo} ${MARKER}"
-    done
-done
-
-# Latin America COVID-19 endpoint: 5 markers, for Overall, Senior, and non-Senior
-for TRIAL in janssen_la_partA janssen_la_partAsenior janssen_la_partAnonsenior
-do
-    for MARKER in Day29bindSpike Day29bindRBD Day29pseudoneutid50 Day29pseudoneutid50la Day29ADCP
-    do
-    sbatch --export=TRIAL=${TRIAL} \
+        done
+      done
+      # SOUTH AFRICA
+      for TRIAL in janssen_sa_partA
+        do
+        for MARKER in Day29bindSpike Day29bindRBD Day29pseudoneutid50 Day29pseudoneutid50sa
+          do
+          sbatch --export=TRIAL=${TRIAL} \
            --wrap="cd ~/correlates_reporting2/cop_mediation && /app/software/R/4.0.4-foss-2020b/bin/Rscript code/mediation.R D29IncludeNotMolecConfirmed ${run_survtmle} ${impute_placebo} ${MARKER}"
-    done
-done
-
-# South Africa COVID-19 endpoint: 5 markers, for Overall, Senior, and non-Senior
-for TRIAL in janssen_sa_partA janssen_sa_partAnonsenior
-do
-    for MARKER in Day29bindSpike Day29bindRBD Day29pseudoneutid50 Day29pseudoneutid50sa Day29ADCP
-    do
-    sbatch --export=TRIAL=${TRIAL} \
+        done
+      done
+      # NORTH AMERICA 
+      for TRIAL in janssen_na_partA
+        do
+        for MARKER in Day29bindSpike Day29bindRBD Day29pseudoneutid50
+          do
+          sbatch --export=TRIAL=${TRIAL} \
            --wrap="cd ~/correlates_reporting2/cop_mediation && /app/software/R/4.0.4-foss-2020b/bin/Rscript code/mediation.R D29IncludeNotMolecConfirmed ${run_survtmle} ${impute_placebo} ${MARKER}"
-    done
-done
-
-# U.S. COVID-19 endpoint: 4 markers, for Overall, Senior, and non-Senior
-for TRIAL in janssen_na_partA janssen_na_partAsenior janssen_na_partAnonsenior  
-do
-    for MARKER in Day29bindSpike Day29bindRBD Day29pseudoneutid50 Day29ADCP
-    do
-    sbatch --export=TRIAL=${TRIAL} \
-           --wrap="cd ~/correlates_reporting2/cop_mediation && /app/software/R/4.0.4-foss-2020b/bin/Rscript code/mediation.R D29IncludeNotMolecConfirmed ${run_survtmle} ${impute_placebo} ${MARKER}"
-    done
-done
-
-# Geog regions pooled severe COVID-19 endpoint: 4 markers for Overall
-for TRIAL in janssen_pooled_partA
-do
-    for MARKER in Day29bindSpike Day29bindRBD Day29pseudoneutid50 Day29ADCP
-    do
-    sbatch --export=TRIAL=${TRIAL} \
-           --wrap="cd ~/correlates_reporting2/cop_mediation && /app/software/R/4.0.4-foss-2020b/bin/Rscript code/mediation.R D29SevereIncludeNotMolecConfirmed ${run_survtmle} ${impute_placebo} ${MARKER}"
-    done
-done
-
-# Latin America severe COVID-19 endpoint: 5 markers for Overall
-for TRIAL in janssen_la_partA
-do
-    for MARKER in Day29bindSpike Day29bindRBD Day29pseudoneutid50 Day29pseudoneutid50la Day29ADCP
-    do
-    sbatch --export=TRIAL=${TRIAL} \
-           --wrap="cd ~/correlates_reporting2/cop_mediation && /app/software/R/4.0.4-foss-2020b/bin/Rscript code/mediation.R D29SevereIncludeNotMolecConfirmed ${run_survtmle} ${impute_placebo} ${MARKER}"
-    done
-done
-
-# AZD 
-for TRIAL in azd1222
-do
-    for MARKER in Day57pseudoneutid50
-    do
-        sbatch --export=TRIAL=${TRIAL} \
-       --wrap="cd ~/correlates_reporting2/cop_mediation && /app/software/R/4.0.4-foss-2020b/bin/Rscript code/mediation.R D57 ${run_survtmle} ${impute_placebo} ${MARKER}"
-    done
-
-    for MARKER in Day29pseudoneutid50
-    do
-        sbatch --export=TRIAL=${TRIAL} \
-       --wrap="cd ~/correlates_reporting2/cop_mediation && /app/software/R/4.0.4-foss-2020b/bin/Rscript code/mediation.R D29 ${run_survtmle} ${impute_placebo} ${MARKER}"
-    done
-done
-
-sbatch --export=TRIAL=azd1222_bAb \
-       --wrap="cd ~/correlates_reporting2/cop_mediation && /app/software/R/4.0.4-foss-2020b/bin/Rscript code/mediation.R D29 ${run_survtmle} ${impute_placebo} Day29bindSpike"
-
-done
-done
+        done
+      done
+    done # END OUTCOME
+  done # END IMPUTE PLACEBO
+done # END SURVTMLE
