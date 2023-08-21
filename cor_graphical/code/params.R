9526773bba021794e8cabbe75a18e088cab8e33a
Yiwen Lu <kylielyw@gmail.com>
2023-08-21 13:03:22 -0700

    remove redundant code

--- cor_graphical/code/params.R
+++ cor_graphical/code/params.R
@@ -2,7 +2,6 @@
 
 library(here)
 library(stringr)
-save.results.to <- here("figs")
 
 labels.title2 <- apply(labels.title, c(1, 2), function(st) {
   str_replace(st, ":", "\n")
@@ -11,7 +10,3 @@ labels.title2 <- apply(labels.title, c(1, 2), function(st) {
 trt.labels <- c("Placebo", "Vaccine")
 bstatus.labels <- c("Baseline Neg", "Baseline Pos")
 bstatus.labels.2 <- c("BaselineNeg", "BaselinePos")
-
-llods <- llods[assays]
-lloqs <- lloqs[assays]
-uloqs <- uloqs[assays]
