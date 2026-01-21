6a2875a9612b91ed03dcc58bb5f9f7d818e19a26
Youyi Fong <youyifong@gmail.com>
2026-01-21 00:03:33 -0800

    use module-level renv to render report

--- cor_poisson/_build_chapter.sh
+++ cor_poisson/_build_chapter.sh
@@ -1,3 +1,4 @@
 export module=cor_poisson
-cd ..
-Rscript -e "bookdown::render_book(input = 'index_cor.Rmd', output_file = 'correlates_${module}_${TRIAL}_$(date +%Y%m%d).pdf',   config_file = '_bookdown_${module}.yml', output_format = bookdown::pdf_document2(toc_depth=5), quiet=TRUE)"	
+Rscript -e "setwd('..'); bookdown::render_book(input = 'index_cor.Rmd', output_file = 'correlates_${module}_${TRIAL}_$(date +%Y%m%d).pdf',   config_file = '_bookdown_${module}.yml', output_format = bookdown::pdf_document2(toc_depth=5), quiet=TRUE)"
+
+
