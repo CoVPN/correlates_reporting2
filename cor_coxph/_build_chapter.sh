0173a0bd40f1a40da3b571d4270bd4e2f4d23d57
Youyi Fong <youyifong@gmail.com>
2026-02-04 12:28:26 -0800

    Render report using the module level renv.lock

--- cor_coxph/_build_chapter.sh
+++ cor_coxph/_build_chapter.sh
@@ -1,18 +1,4 @@
 export module=cor_coxph
-cd ..
-if [ $TRIAL = "vat08_combined" ]; then
 
-  if [ $stage = 1 ]; then
-    echo "stage 1"
-	Rscript -e "setwd('..'); bookdown::render_book(input = 'index_cor.Rmd', output_file = 'covpn_correlates_${module}_vat08_stage1_$(date +%Y%m%d).pdf', config_file = '_bookdown_${module}.yml', output_format = bookdown::pdf_document2(toc_depth=5), quiet=TRUE)"	
-  else
-    echo "stage 2"
-	Rscript -e "setwd('..'); bookdown::render_book(input = 'index_cor.Rmd', output_file = 'covpn_correlates_${module}_vat08_stage2_$(date +%Y%m%d).pdf', config_file = '_bookdown_${module}.yml', output_format = bookdown::pdf_document2(toc_depth=5), quiet=TRUE)"	
-  fi
-  cd $module
+Rscript -e "bookdown::render_book(input = 'index_cor.Rmd', output_file = 'covpn_correlates_${module}_${TRIAL}_$(date +%Y%m%d).pdf',   config_file = '_bookdown_${module}.yml', output_format = bookdown::pdf_document2(toc_depth=5), quiet=TRUE)"	
 
-else 
-
-	Rscript -e "setwd('..'); bookdown::render_book(input = 'index_cor.Rmd', output_file = 'covpn_correlates_${module}_${TRIAL}_$(date +%Y%m%d).pdf',   config_file = '_bookdown_${module}.yml', output_format = bookdown::pdf_document2(toc_depth=5), quiet=TRUE)"	
-
-fi
